77297055ba4d74cd1ca65f6de742d019eb1dbd3e
Kapil Joshi <kapiljoshi795@gmail.com>
2020-05-05 15:49:45 +0530

    changes

--- flights-api/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO FLIGHTS (flight_id, flight_name, departure, arrival, arrival_time, departure_time, seats_avl) VALUES
-  (1001, 'Vistara UK-963', 'DEL', 'NYK', TIMESTAMP '2020-04-22 22:40:00', 
-   TIMESTAMP '2020-04-22 08:45:00', 250);
- 
-			
